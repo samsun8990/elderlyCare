b705afcf45001d0493ddc5eb0e4cd121d22c480f
heba600 <60092554@udst.edu.qa>
2023-11-14 10:14:53 +0300

    elderProfile

---
+++ components/Pofile/ElderProfile.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const ElderProfile = () => {
+  return (
+    <View>
+      <Text>ElderProfile</Text>
+    </View>
+  )
+}
+
+export default ElderProfile
+
+const styles = StyleSheet.create({})
